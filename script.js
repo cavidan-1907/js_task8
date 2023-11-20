8ad9652949ce41938349ee6b7c48684c5a98b11c
cavidan-1907 <cavidanvelizade9@gmail.com>
2023-11-20 22:09:54 +0400

    forEach

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const boxes = document.querySelectorAll(".box");
 
-let func = function (boxs) {
-    for (let i = 0; i < boxs.length; i++) {
-        const box = boxs[i];
+// let func = function (boxs) {
+//     for (let i = 0; i < boxs.length; i++) {
+//         const box = boxs[i];
 
-        if ((i + 1) % 5 === 0) {
-            box.style.backgroundColor = "green";
-        } else if ((i+1) % 2 === 0) {
-            box.style.backgroundColor = "navy";
-        } else {
-            box.style.backgroundColor = "yellow";
-        }
+//         if ((i + 1) % 5 === 0) {
+//             box.style.backgroundColor = "green";
+//         } else if ((i+1) % 2 === 0) {
+//             box.style.backgroundColor = "navy";
+//         } else {
+//             box.style.backgroundColor = "yellow";
+//         }
+//     }
+// };
+
+// func(boxes);
+//foreachla
+
+boxes.forEach((box, i) => {
+    if ((i + 1) % 5 === 0) {
+        box.style.backgroundColor = "green";
+        box.innerHTML="5"
+    } else if ((i + 1) % 2 === 0) {
+        box.style.backgroundColor = "navy";
+        box.innerHTML="2"
+    } else {
+        box.style.backgroundColor = "yellow";
+        box.innerHTML="1"
     }
-};
-
-func(boxes);
+});
